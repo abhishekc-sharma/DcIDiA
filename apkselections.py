8e99ba7d14a5bab707c7139cf1cf44db8c442f4d
panjaygau <35715851+panjaygau@users.noreply.github.com>
2018-04-09 17:22:37 +0200

    Add files via upload

---
+++ apkselections.py
@@ -0,0 +1,28 @@
+import os, sys
+import shutil
+import random
+from shutil import copyfile
+try:
+	sys.argv[1]
+except IndexError:
+	print("Please provide source path to dataset")
+	sys.exit(1)
+
+source_dir = sys.argv[1]
+
+try:
+	sys.argv[2]
+except IndexError:
+	print("Please provide destination path")
+	sys.exit(1)
+dest_dir = sys.argv[2]
+for fam in os.listdir(source_dir):
+	os.makedirs(dest_dir+fam+"/")
+	for var in os.listdir(source_dir+fam+"/"):
+		os.makedirs(dest_dir+fam+"/"+var+"/")
+		if(len(os.listdir(source_dir+fam+"/"+var+"/")) < 20):
+			filenames = random.sample(os.listdir(source_dir+fam+"/"+var+"/" ), len(os.listdir(source_dir+fam+"/"+var+"/")))
+		else:
+			filenames = random.sample(os.listdir(source_dir+fam+"/"+var+"/" ), 20)
+		for j in filenames:
+			shutil.copy2(source_dir+fam+"/"+var+"/"+j,dest_dir+fam+"/"+var+"/"+j)
